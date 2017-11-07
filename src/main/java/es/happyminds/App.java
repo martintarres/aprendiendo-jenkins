fd4478d91a588725179f4afe27184f06af51b0db
martin <martin@martin>
2017-11-07 16:57:27 -0300

    le pongo mas espacios

--- src/main/java/es/happyminds/App.java
+++ src/main/java/es/happyminds/App.java
@@ -6,7 +6,8 @@ package es.happyminds;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args
+ )
     {
         System.out.println( "Hello World!" )
 
